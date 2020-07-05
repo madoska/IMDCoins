a23f628978ae944b29f0dc81a87625039c0690b0
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-07-05 16:30:21 +0200

    fixed database to return json msg with success or failed to connect

--- classes/Db.php
+++ classes/Db.php
@@ -1,10 +1,21 @@
 <?php
-class Db {
-    public static function connect(){
-        include_once(__DIR__."/../settings/settings.php");
+class Db
+{
+    public static function connect()
+    {
+        try {
+            include_once(__DIR__ . "/../settings/settings.php");
 
-        $dsn = 'mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['db'];
-        $pdo = new PDO($dsn, SETTINGS['db']['user'], SETTINGS['db']['password']);
-        return $pdo;
+            $dsn = 'mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['db'];
+            $pdo = new PDO(
+                $dsn,
+                SETTINGS['db']['user'],
+                SETTINGS['db']['password'],
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+            );
+            die(json_encode(array('outcome' => true, 'message' => 'Connected')));
+        } catch (PDOException $ex) {
+            die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+        }
     }
 }
