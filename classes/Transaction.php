7a45dafd32f62aee084eb91b208af3ce30a35b13
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-07-05 23:22:03 +0200

    created function to send free tokens upon registration

---
+++ classes/Transaction.php
@@ -0,0 +1,53 @@
+<?php
+include_once(__DIR__ . "/Db.php");
+
+class Transaction
+{
+    private $userID;
+    private $sum;
+    private $msg;
+
+    public function activationTokens($userID){
+        $pdo = Db::connect();
+        $stmt = $pdo->prepare("INSERT INTO transactions (senderID, recipientID, amount, message) VALUES (1, :recipientID, 10, 'A little token of gratitude for joining us! :)')");
+        $stmt->bindParam(':recipientID', $userID);
+        $result = $stmt->execute();
+        return $result;
+    }
+
+    public function getUserID()
+    {
+        return $this->userID;
+    }
+
+    public function setUserID($userID)
+    {
+        $this->userID = $userID;
+
+        return $this;
+    }
+
+    public function getSum()
+    {
+        return $this->sum;
+    }
+ 
+    public function setSum($sum)
+    {
+        $this->sum = $sum;
+
+        return $this;
+    }
+
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+
+        return $this;
+    }
+}
