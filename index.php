7f5917e932891f3d90954839828852a6d492ad5b
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-07-10 18:05:15 +0200

    changed stylesheet link

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ $sum = $saldo->saldo($userID);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>IMDCurrency</title>
 </head>
 
