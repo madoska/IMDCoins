c1fea3c73280d3e1c4ee0c0dd7eeedb361b57d08
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-07-10 16:55:29 +0200

    login styling complete

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ include_once(__DIR__ . "/classes/User.php");
 session_start();
 session_destroy();
 
-if(!empty($_POST['login'])){
+if (!empty($_POST['login'])) {
     $validateLogin = new User();
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -12,7 +12,7 @@ if(!empty($_POST['login'])){
     $validateLogin->setPassword($password);
     $result = $validateLogin->validateLogin($email, $password);
 
-    if($result == 1){
+    if ($result == 1) {
         session_start();
         $userID = $validateLogin->userID($email);
         $_SESSION['user'] = $userID;
@@ -27,25 +27,45 @@ if(!empty($_POST['login'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/register.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
     <title>Login</title>
 </head>
 
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div>
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email">
+    <div class="d-md-flex h-md-100 align-items-center">
+        <div class="col-md-6 p-0 h-md-100 brandingarea">
+            <div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
+                <img class="logo" src="images/logo_white.svg" alt="">
+            </div>
         </div>
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
+        <div class="col-md-6 p-0 bg-white h-md-100 signuparea">
+            <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+                <div class="flex-box">
+                    <h1 class="title">Login</h1>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div>
+                            <input type="text" name="email" id="email" placeholder="Email">
+                        </div>
 
-        <div>
-            <input type="submit" value="Sign in" name="login" id="login">
+                        <div>
+                            <input type="password" name="password" id="password" placeholder="Password">
+                        </div>
+
+                        <div>
+                            <input type="submit" value="Sign in" name="login" id="login" class="shadow cta">
+                        </div>
+                    </form>
+                    <div class="flex-tiny">
+                        <p class="tiny">Don't have an account?</p><a href="register.php" class="login">Sign up</a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
+    </div>
 </body>
 
 </html>
