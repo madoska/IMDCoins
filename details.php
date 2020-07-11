0b0b90881d18a17ce7ea9b3d7951fe564fa1fb65
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-07-11 19:45:31 +0200

    details.php complete, styling not done yet

--- details.php
+++ details.php
@@ -27,7 +27,23 @@ $transactions = $history->history($userID);
             <div class="text-black h-100 p-5">
                 <h1>Transfer receipt</h1>
                 <div class="columns">
-                    
+                    <ul class="labels">
+                        <li>Transaction date</li>
+                        <li>Sender</li>
+                        <li>Recipient</li>
+                        <li>Amount</li>
+                        <li>Message</li>
+                    </ul>
+                    <ul class="variables">
+                        <?php foreach($transactions as $trans): ?>
+                            <?php if($trans['transID'] == $transID){?>
+                                <li><?php echo $trans['time']; ?></li>
+                                <li><?php echo $trans['sender_firstname'] . " " . $trans['sender_lastname']; ?></li>
+                                <li><?php echo $trans['recipient_firstname'] . " " . $trans['recipient_lastname']; ?></li>
+                                <li><?php echo $trans['amount'] . " tokens"; ?></li>
+                                <li><?php echo $trans['message']; ?></li>
+                            <?php } endforeach; ?>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -40,9 +56,15 @@ $transactions = $history->history($userID);
                     foreach ($transactions as $trans) : ?>
                         <?php
                         if ($trans['recipientID'] == $userID) { ?>
-                            <li class="transItems <?php if($transID == $trans['transID']){ echo "selected"; } else {}; ?>"><?php echo  $trans['sender_firstname'] . " sent you " . $trans['amount'] . " tokens"; ?><a class="transMore" href="details.php?id=<?php echo $trans['transID']; ?>">></a></li>
+                            <li class="transItems <?php if ($transID == $trans['transID']) {
+                                                        echo "selected";
+                                                    } else {
+                                                    }; ?>"><?php echo  $trans['sender_firstname'] . " sent you " . $trans['amount'] . " tokens"; ?><a class="transMore" href="details.php?id=<?php echo $trans['transID']; ?>">></a></li>
                         <?php } else { ?>
-                            <li class="transItems <?php if($transID == $trans['transID']){ echo "selected"; } else {}; ?>"><?php echo "You sent " . $trans['recipient_firstname'] . " " . $trans['amount'] . " tokens"; ?><a class="transMore" href="details.php?id=<?php echo $trans['transID']; ?>">></a></li>
+                            <li class="transItems <?php if ($transID == $trans['transID']) {
+                                                        echo "selected";
+                                                    } else {
+                                                    }; ?>"><?php echo "You sent " . $trans['recipient_firstname'] . " " . $trans['amount'] . " tokens"; ?><a class="transMore" href="details.php?id=<?php echo $trans['transID']; ?>">></a></li>
                         <?php } ?>
                     <?php endforeach; ?>
                 </ul>
