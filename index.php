c5d69ed7e7c76db0e00959c7a4163a9ead830f0d
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-07-11 22:47:29 +0200

    changed doc title

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $transactions = $history->history($userID);
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <title>IMDCurrency</title>
+    <title>Coins</title>
 </head>
 
 <body>
