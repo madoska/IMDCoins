5bd215ac08499fbd4c77d08e228784b816f98663
madoska <medina-dadurgova@hotmail.com>
2020-07-26 22:30:52 +0200

    added hidden field with transID

--- details.php
+++ details.php
@@ -54,7 +54,8 @@ $transactions = $history->history($userID);
         <div class="col-md-4 p-0 bg-white h-md-100">
             <div class="h-md-100 p-5 brandingarea">
                 <h2 class="history">History</h2>
-                <ul class="listitems">
+                <input type="hidden" id="hidden" name="hidden" value="<?php echo $userID; ?>">
+                <ul id="listitems" class="listitems">
                     <?php
                     foreach ($transactions as $trans) : ?>
                         <?php
@@ -62,12 +63,87 @@ $transactions = $history->history($userID);
                             <li class="transItems"><a class="transLink <?php if ($transID == $trans['transID']) {echo "selected";} else {}; ?>" href="details.php?id=<?php echo $trans['transID']; ?>"><?php echo  $trans['sender_firstname'] . " sent you " . $trans['amount'] . " tokens"; ?></a><a class="transMore <?php if ($transID == $trans['transID']) {echo "selected";} else {}; ?>" href="details.php?id=<?php echo $trans['transID']; ?>">></a></li>
                         <?php } else { ?>
                             <li class="transItems"><a class="transLink <?php if ($transID == $trans['transID']) {echo "selected";} else {}; ?>" href="details.php?id=<?php echo $trans['transID']; ?>"><?php echo "You sent " . $trans['recipient_firstname'] . " " . $trans['amount'] . " tokens"; ?></a><a class="transMore <?php if ($transID == $trans['transID']) {echo "selected";} else {}; ?>" href="details.php?id=<?php echo $trans['transID']; ?>">></a></li>
-                        <?php } ?>
-                    <?php endforeach; ?>
+                        <?php }
+
+                        if($transID == $trans['transID']){
+                            echo '<input type="hidden" id="transHidden" name="hidden" value ="' . $trans['transID'] . '">';
+                        } else {}   
+                    endforeach; ?>
                 </ul>
             </div>
         </div>
     </div>
+
+    <script>
+        window.onload = timer;
+
+        function timer(){
+            setInterval(() => {
+                updateHistory()
+            }, 3000);
+        }
+        
+        const userID = document.getElementById("hidden").value;
+        console.log(userID);
+
+        const transID = document.getElementById("transHidden").value;
+        console.log(transID);
+
+        function updateHistory(){
+            let formData = new FormData();
+            formData.append('userID', userID);
+
+            fetch('ajax/fetchHistory.php', {
+                method: 'POST',
+                body: formData
+            })
+
+            .then(response => response.json())
+            .then(result => viewHistory(result))
+            .catch(error => {
+                console.log('Error: ', error);
+            });
+        }
+
+        function viewHistory(result){
+            const history = document.getElementById("listitems");
+            history.innerHTML = "";
+
+            for(let i = 0; i < result.length; i++){
+                if(result[i].senderID == userID){
+                    let a = document.createElement("a");
+                    let li = document.createElement("li");
+                    let href = "details.php?id=" + result[i].transID;
+                    let title = "You sent " + result[i].recipient_firstname + " " + result[i].amount + " tokens.";
+
+                    li.classList.add("transItems");
+                    a.classList.add("transLink");
+
+                    a.textContent = title;
+                    a.setAttribute('href', href);
+                    li.appendChild(a);
+                    history.appendChild(li);
+
+                    console.log("history items updated");
+                } else {
+                    let a = document.createElement("a");
+                    let li = document.createElement("li");
+                    let href = "details.php?id=" + result[i].transID;
+                    let title = result[i].sender_firstname + " sent you " + result[i].amount + " tokens.";
+
+                    li.classList.add("transItems");
+                    a.classList.add("transLink");
+
+                    a.textContent = title;
+                    a.setAttribute('href', href);
+                    li.appendChild(a);
+                    history.appendChild(li);
+
+                    console.log("history items updated");
+                }
+            }
+        }
+    </script>
 </body>
 
 </html>
